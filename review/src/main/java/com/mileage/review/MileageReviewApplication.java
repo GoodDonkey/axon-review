bc18bb50620e67666bc0914e6b743d947465884a
GoodDonkey <president20500@gmail.com>
2022-07-15 01:35:51 +0900

    chore: review project setting

---
+++ review/src/main/java/com/mileage/review/MileageReviewApplication.java
@@ -0,0 +1,11 @@
+package com.mileage.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MileageReviewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MileageReviewApplication.class, args);
+    }
+}
