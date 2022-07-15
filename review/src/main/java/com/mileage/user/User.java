bac28cb0f5a204ad49d8e067a7776e7e8035325b
GoodDonkey <president20500@gmail.com>
2022-07-15 16:35:13 +0900

    feat: added user as jpa entity

--- review/src/main/java/com/mileage/user/User.java
+++ review/src/main/java/com/mileage/user/User.java
@@ -7,8 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
-import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 import static org.axonframework.modelling.command.AggregateLifecycle.apply;
 
@@ -16,7 +18,9 @@ import static org.axonframework.modelling.command.AggregateLifecycle.apply;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Slf4j
+@Entity
 public class User {
+    @Id
     @AggregateIdentifier
     private String userId;
     
