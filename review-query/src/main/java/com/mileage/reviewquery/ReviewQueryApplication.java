c664e110a448473f2d43cd8a9eeb1b1af2c82f62
GoodDonkey <president20500@gmail.com>
2022-07-18 16:00:54 +0900

    feat: review-query project setting

---
+++ review-query/src/main/java/com/mileage/reviewquery/ReviewQueryApplication.java
@@ -0,0 +1,12 @@
+package com.mileage.reviewquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewQueryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewQueryApplication.class, args);
+    }
+    
+}
