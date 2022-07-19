7659b496bc679314454ad26f8a6ffabfae52eabd
GoodDonkey <president20500@gmail.com>
2022-07-20 01:32:00 +0900

    feat: Error response body

--- review/src/main/java/com/mileage/review/web/ReviewControllerAdvice.java
+++ review/src/main/java/com/mileage/review/web/ReviewControllerAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
@@ -13,20 +14,20 @@ import java.util.concurrent.TimeoutException;
 public class ReviewControllerAdvice {
     
     @ExceptionHandler(ExecutionException.class)
-    public ResponseEntity<ExceptionMessage> on(ExecutionException exception) {
+    public ResponseEntity<ErrorResponseBody> on(ExecutionException exception, HttpServletRequest request) {
         log.error("exception: {}", exception.getMessage());
-        return ResponseEntity.internalServerError().body(ExceptionMessage.of(exception));
+        return ResponseEntity.internalServerError().body(ErrorResponseBody.of(exception, request.getRequestURI()));
     }
     
     @ExceptionHandler(InterruptedException.class)
-    public ResponseEntity<ExceptionMessage> on(InterruptedException exception) {
+    public ResponseEntity<ErrorResponseBody> on(InterruptedException exception, HttpServletRequest request) {
         log.error("exception: {}", exception.getMessage());
-        return ResponseEntity.internalServerError().body(ExceptionMessage.of(exception));
+        return ResponseEntity.internalServerError().body(ErrorResponseBody.of(exception, request.getRequestURI()));
     }
     
     @ExceptionHandler(TimeoutException.class)
-    public ResponseEntity<ExceptionMessage> on(TimeoutException exception) {
+    public ResponseEntity<ErrorResponseBody> on(TimeoutException exception, HttpServletRequest request) {
         log.error("exception: {}", exception.getMessage());
-        return ResponseEntity.internalServerError().body(ExceptionMessage.of(exception));
+        return ResponseEntity.internalServerError().body(ErrorResponseBody.of(exception, request.getRequestURI()));
     }
 }
